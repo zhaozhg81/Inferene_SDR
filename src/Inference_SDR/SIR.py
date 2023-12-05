df7ef2a4d73640b8a9dc88f86e05d4fdfdc114d8
Zhigen Zhao <zhigen.zhao@gmail.com>
2023-12-05 14:42:29 -0500

    update SIR.py

--- src/Inference_SDR/SIR.py
+++ src/Inference_SDR/SIR.py
@@ -33,9 +33,77 @@ class SIR(SDR):
         LambdaHat = np.dot(x_sliced_mean.T, x_sliced_mean) / self.H
         eigenvalues, eigenvectors = np.linalg.eig(LambdaHat)
         self.res_sir = eigenvectors[:, :self.D]
+        for d in range(self.D):
+            self.res_sir[:,d] = self.res_sir[:,d]/np.dot( self.res_sir[:,d], self.res_sir[:,d])s
+
+    def sir_direction(X,Y,n, p, H,numdir):
         
+        m = int(n/H)
+        x_sliced_mean = np.zeros((H, X.shape[1]))
+        
+        ORD = np.argsort(Y, axis=0 )
+        grand_mean = np.mean(X, axis=0)
+        
+        for h in range(H):
+            x_sliced = X[ORD[(h*m):((h+1)*m),0], :]
+            x_sliced_mean[h, :] = np.mean(x_sliced, axis=0) - grand_mean
+        
+        LambdaHat = np.dot(x_sliced_mean.T, x_sliced_mean) /H
+        
+        eigenvalues, eigenvectors = np.linalg.eig(LambdaHat)
+        res_sir = eigenvectors[:, :D]
+        for d in range(D):
+            res_sir[:,d] = res_sir[:,d]/np.dot( res_sir[:,d], res_sir[:,d])s
 
     
     def predict_SDR(self, X_test):        
         X_pred = np.matmul(X_test, self.res_sir)
         return( X_pred )
+    
+    
+def Lasso_SIR(X,Y,n,p,H,numdir):
+    screening = False 
+    
+    ORD = np.argsort(Y, axis=0)[:,0]
+    X_tran = X[ORD, :]
+    Y_tran = Y[ORD]
+    
+    ms = np.zeros(n)
+    m = n // H
+    c = n % H
+    M = np.zeros((H, n))
+    if c == 0:
+        M = np.kron(np.eye(H), np.ones((1, m))) / m
+    else:
+        for i in range(1, c+1):
+            M[i-1, (m+1)*(i-1): (m+1)*i] = 1 / (m+1)
+            ms[(m+1)*(i-1): (m+1)*i] = m
+        for i in range(c+1, H+1):
+            M[i-1, (m+1)*c + (i-c-1)*m: (m+1)*c + (i-c)*m] = 1 / m
+            ms[(m+1)*c + (i-c-1)*m: (m+1)*c + (i-c)*m] = m - 1
+    
+    if screening:
+        x_sliced_mean = np.matmul(M, X_tran)
+        sliced_variance = np.apply_along_axis(np.var, 0, x_sliced_mean)
+        keep_ind = np.argsort(sliced_variance)[::-1][:n]
+    else:
+        keep_ind = np.arange(0, p)
+    
+    X_tran = X_tran[:, keep_ind]
+    X_H = np.zeros((H, X.shape[1]))
+    grand_mean = np.mean(X, axis=0)
+    X_stand_ord = X_tran - np.kron( grand_mean.reshape(1, -1), np.ones((X.shape[0], 1)) )
+    X_H = np.matmul( M , X_stand_ord )
+    
+    lambda_H = np.dot(X_H.T, X_H)/H
+    eigenvalues, eigenvectors = np.linalg.eig(lambda_H)
+    
+    
+    beta_hat = np.zeros((p, no_dim))
+    Y_tilde = np.zeros((n, no_dim))
+
+    
+    for ii in range(0, no_dim):
+        Y_tilde[:, ii] = m* np.matmul(np.matmul(np.matmul(M.T, M), X_stand_ord), eigenvectors[ii,:]) / eigenvalues[ii] 
+    
+
--- src/Inference_SDR/example.py
+++ src/Inference_SDR/example.py
@@ -7,8 +7,11 @@ Created on Thu Nov 23 05:47:22 2023
 """
 
 import numpy as np
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from Inference_SDR.SIR import SIR
+import matplotlib.pyplot as plt
+import statistics
 
 
 def add_one(number):
@@ -41,19 +44,30 @@ def generate_demo_data(n, p, H):
     
     return(X, Y, beta)
     
-def demo():
-    n=500
-    p=10
-    H=20
-    m=25
-    
-    
-    X, Y, beta = generate_demo_data(n,p,H)   
+n=500
+p=10
+H=20
+m=25
 
-    # Split the data into training and testing sets
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-    sir_object = SIR(X_train, Y_train, X_train.shape[0], p, H, 1)
-    sir_object.get_direction()
-    
-    X_pred = sir_object.predict_SDR( X_test)
 
+X, Y, beta = generate_demo_data(n,p,H)   
+
+# Split the data into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+sir_object = SIR(X_train, Y_train, X_train.shape[0], p, H, 1)
+sir_object.get_direction()
+
+X_train_pred = sir_object.predict_SDR(X_train)
+X_test_pred = sir_object.predict_SDR( X_test)
+
+
+# Create the model
+model = Ridge()
+
+# Fit the model to the data
+model.fit(X_train_pred, Y_train)
+
+# Predict the labels for new data
+Y_test_pred = model.predict(X_test_pred)
+
+MSE = statistics.mean( ( (Y_test_pred-Y_test)*(Y_test_pred-Y_test)  )[:,0] )
